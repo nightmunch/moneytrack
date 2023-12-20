1355438e82e69e2f3541eee1ee3870cef5e3ccdc
Shahrin Amin <shahrinamin.my@gmail.com>
2023-12-20 13:43:02 +0800

    fix: handle original hsl format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import { ModeToggle } from "@/components/ui/mode-toggle";
 export default function Home() {
   return (
     <div>
-      <ModeToggle />
+      {/* Navbar */}
+      <div className="fixed left-0 right-0 top-0 z-50">
+        {/* Navbar goes here */}
+        <div className="h-16 bg-white dark:bg-slate-950"></div>
+        <ModeToggle />
+      </div>
     </div>
   );
 }
