e64f02be4ca47e943f3ba8fbb7db64f76d456f4b
Shahrin Amin <shahrinamin.my@gmail.com>
2023-12-22 09:37:24 +0800

    fix: add minus to emphasize reduce money

--- src/app/transactions/columns.tsx
+++ src/app/transactions/columns.tsx
@@ -64,7 +64,7 @@ export const columns: ColumnDef<Transaction>[] = [
 
       return (
         <div className="text-right font-semibold text-destructive">
-          {formattedWithCustomCurrency}
+          - {formattedWithCustomCurrency}
         </div>
       );
     },
