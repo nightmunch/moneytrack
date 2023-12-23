aa54eb5bd5cfd786795e22e9300e90304f75216c
Shahrin Amin <shahrinamin.my@gmail.com>
2023-12-23 12:04:13 +0800

    feat: ui buttons for claims

--- src/app/claims/page.tsx
+++ src/app/claims/page.tsx
@@ -1,7 +1,8 @@
-import { date } from "zod";
+import { Button } from "@/components/ui/button";
 import { type Claim, columns } from "./columns";
 import { DataTable } from "./data-table";
 import ClientOnly from "@/components/client-only";
+import { Plus, Sheet } from "lucide-react";
 
 async function getData(): Promise<Claim[]> {
   // Fetch data from your API here.
@@ -36,7 +37,23 @@ export default async function Claims() {
 
   return (
     <div className="flex flex-col gap-3 px-10 py-4">
-      <h1 className="text-xl font-semibold text-primary">Claims</h1>
+      <div className="pb-2">
+        <h1 className="text-xl font-semibold text-primary">Claims</h1>
+        <h2 className="text-sm text-muted-foreground">
+          Here's the list of all your claims.
+        </h2>
+      </div>
+      <div className="flex justify-end gap-2">
+        <Button variant={"outline"} size={"icon"}>
+          <Sheet className="h-4 w-4" />
+          <span className="sr-only">Download to Excel</span>
+        </Button>
+        <Button variant={"outline"}>Claim All</Button>
+        <Button variant={"default"} size={"icon"}>
+          <Plus className="h-4 w-4" />
+          <span className="sr-only">Add Claim</span>
+        </Button>
+      </div>
       <ClientOnly>
         <DataTable columns={columns} data={data} />
       </ClientOnly>
