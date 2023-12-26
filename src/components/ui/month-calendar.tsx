1d28c589e9a26cea7f14648de8c3e23020df245b
Shahrin Amin <shahrinamin.my@gmail.com>
2023-12-27 00:37:49 +0800

    feat: month calendar is now super aria!

--- src/components/ui/month-calendar.tsx
+++ src/components/ui/month-calendar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { Button } from "./button";
+import { useRef, useEffect } from "react";
 
 export const MonthCalendar = ({
   date,
@@ -48,20 +49,95 @@ export const MonthCalendar = ({
       return "ghost";
     }
   }
+
+  const gridRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const currentActiveElement = document.activeElement as HTMLElement;
+      const gridElement = gridRef.current;
+
+      if (
+        gridElement &&
+        currentActiveElement &&
+        currentActiveElement.parentElement === gridElement
+      ) {
+        const buttonElements = gridElement.querySelectorAll("button");
+        const currentButtonIndex = Array.from(buttonElements).findIndex(
+          (button) => button === currentActiveElement,
+        );
+
+        if (event.key === "ArrowUp") {
+          event.preventDefault();
+          const previousButton = buttonElements[currentButtonIndex - 3];
+          if (previousButton) {
+            previousButton.focus();
+          } else {
+            updateDate(-1);
+          }
+        } else if (event.key === "ArrowDown") {
+          event.preventDefault();
+          const nextButton = buttonElements[currentButtonIndex + 3];
+          if (nextButton) {
+            nextButton.focus();
+          } else {
+            updateDate(1);
+          }
+        } else if (event.key === "ArrowLeft") {
+          event.preventDefault();
+          const previousButton = buttonElements[currentButtonIndex - 1];
+          if (previousButton) {
+            previousButton.focus();
+          } else {
+            updateDate(-1);
+          }
+        } else if (event.key === "ArrowRight") {
+          event.preventDefault();
+          const nextButton = buttonElements[currentButtonIndex + 1];
+          if (nextButton) {
+            nextButton.focus();
+          } else {
+            updateDate(1);
+          }
+        }
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [date]);
+
   return (
     <div className="flex flex-col gap-2">
       <div className="flex items-center justify-between">
-        <Button variant="ghost" size="icon-sm" onClick={() => updateDate(-1)}>
+        <Button
+          variant="ghost"
+          size="icon-sm"
+          onClick={() => updateDate(-1)}
+          aria-label="Previous year"
+        >
           <ChevronLeftIcon className="h-4 w-4" />
         </Button>
         <h1 className="text-sm font-semibold">
           {date ? date.getFullYear() : currentYear}
         </h1>
-        <Button variant="ghost" size="icon-sm" onClick={() => updateDate(1)}>
+        <Button
+          variant="ghost"
+          size="icon-sm"
+          onClick={() => updateDate(1)}
+          aria-label="Next year"
+        >
           <ChevronRightIcon className="h-4 w-4" />
         </Button>
       </div>
-      <div className="grid grid-cols-3 text-center">
+      <div
+        className="grid grid-cols-3 grid-rows-4 text-center"
+        role="grid"
+        ref={gridRef}
+      >
         {months.map((month, index) => (
           <Button
             variant={getMonthVariant(date, index, currentMonth, currentYear)}
@@ -76,6 +152,8 @@ export const MonthCalendar = ({
                     : new Date(currentYear, index),
               )
             }
+            aria-label={month}
+            role="gridcell"
           >
             {month}
           </Button>
