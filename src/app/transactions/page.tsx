2c3035b44c165fafc6c5fcc35e76ad3e226842a6
Shahrin Amin <shahrinamin.my@gmail.com>
2023-12-27 07:44:55 +0800

    feat: add loading for transactions clientonly too

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -2,6 +2,7 @@ import { MonthPicker } from "@/components/ui/month-picker";
 import { type Transaction, columns } from "./columns";
 import { DataTable } from "./data-table";
 import ClientOnly from "@/components/client-only";
+import { Skeleton } from "@/components/ui/skeleton";
 
 async function getData(): Promise<Transaction[]> {
   // Fetch data from your API here.
@@ -36,11 +37,25 @@ export default async function Transactions() {
 
   return (
     <div className="flex flex-col gap-3 px-10 py-4">
-      <h1 className="text-xl font-semibold text-primary">Transactions</h1>
+      <div className="pb-2">
+        <h1 className="text-xl font-semibold text-primary">Transactions</h1>
+        <h2 className="text-sm text-muted-foreground">
+          Here's the list of all your transactions.
+        </h2>
+      </div>
       <MonthPicker />
-      <ClientOnly>
+      <ClientOnly LoadingComponent={<Loading />}>
         <DataTable columns={columns} data={data} />
       </ClientOnly>
+    </div>
+  );
+}
+
+function Loading() {
+  return (
+    <div className="grid grid-flow-row gap-2">
+      <Skeleton className="h-8 w-full" />
+      <Skeleton className="h-8 w-full" />
     </div>
   );
 }
