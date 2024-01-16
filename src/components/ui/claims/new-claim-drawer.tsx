e88b689f0979f62412e5453b23dbaa2abeeb42d9
nightmunch <shahrinamin.my@gmail.com>
2024-01-16 09:58:59 +0800

    feat: add react hook form and zod

--- src/components/ui/claims/new-claim-drawer.tsx
+++ src/components/ui/claims/new-claim-drawer.tsx
@@ -22,10 +22,20 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Plus } from "lucide-react";
 import { Separator } from "../separator";
 import { DatePicker } from "../date-picker";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export function NewClaimDrawer() {
   const [open, setOpen] = useState(false);
@@ -37,8 +47,10 @@ export function NewClaimDrawer() {
   if (!isMobile) {
     return (
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger>
-          <OpenButton />
+        <DialogTrigger asChild>
+          <Button variant={"default"} size={"icon"}>
+            <OpenAddClaimButton />
+          </Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
@@ -54,8 +66,10 @@ export function NewClaimDrawer() {
 
   return (
     <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger>
-        <OpenButton />
+      <DrawerTrigger asChild>
+        <Button variant={"default"} size={"icon"}>
+          <OpenAddClaimButton />
+        </Button>
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader className="text-left">
@@ -75,32 +89,111 @@ export function NewClaimDrawer() {
 }
 
 function AddClaimForm({ className }: React.ComponentProps<"form">) {
-  const [date, setDate] = useState<Date | undefined>(new Date());
+  const formSchema = z.object({
+    item: z.string().min(2, {
+      message: "Item name must be at least 2 characters",
+    }),
+    amount: z.number({
+      invalid_type_error: "Amount must be a number",
+      required_error: "Amount is required",
+    }),
+    date: z.date({
+      invalid_type_error: "Date must be a valid date",
+      required_error: "Date is required",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      item: "",
+      date: new Date(),
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
-    <form className={cn("grid items-start gap-4", className)}>
-      <div className="grid gap-2">
-        <Label htmlFor="item">Item</Label>
-        <Input type="text" id="item" placeholder="zus kopi" />
-      </div>
-      <div className="grid gap-2">
-        <Label htmlFor="amount">Amount</Label>
-        <div className="flex items-center gap-2">
-          <span>RM</span>
-          <Input type="number" id="amount" placeholder="10.00" />
+    <Form {...form}>
+      <form
+        className={cn("grid items-start gap-4", className)}
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="grid gap-2">
+          <FormField
+            name="item"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Item</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    id="item"
+                    placeholder="zus kopi"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
-      </div>
-      <div className="grid gap-2">
-        <Label htmlFor="date">Date</Label>
-        <DatePicker date={date} setDate={setDate} />
-      </div>
-      <Button type="submit">Save changes</Button>
-    </form>
+        <div className="grid gap-2">
+          <FormField
+            name="amount"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Amount</FormLabel>
+                <FormControl>
+                  <div className="flex items-center gap-2">
+                    <span>RM</span>
+                    <Input
+                      type="number"
+                      id="amount"
+                      placeholder="10.00"
+                      {...field}
+                      onChange={(event) =>
+                        field.onChange(event.target.valueAsNumber)
+                      }
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid gap-2">
+          <FormField
+            name="date"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Date</FormLabel>
+                <FormControl>
+                  <DatePicker date={field.value} setDate={field.onChange} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit">Save changes</Button>
+      </form>
+    </Form>
   );
 }
 
-const OpenButton = ({ className }: React.ComponentProps<"button">) => (
-  <Button variant={"default"} size={"icon"} className={className}>
-    <Plus className="h-4 w-4" />
-    <span className="sr-only">Add Claim</span>
-  </Button>
-);
+const OpenAddClaimButton = () => {
+  return (
+    <>
+      <Plus className="h-4 w-4" />
+      <span className="sr-only">Add Claim</span>
+    </>
+  );
+};
--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -25,7 +25,7 @@ export function DatePicker({
         <Button
           variant={"outline"}
           className={cn(
-            "justify-start text-left font-normal",
+            "w-full justify-start text-left font-normal",
             !date && "text-muted-foreground",
           )}
         >
